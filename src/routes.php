401974f4bb26164ab7b92c6faf8bc6bb34b51dda
basemkhirat <basemkhirat@gmail.com>
2017-10-05 23:14:32 +0200

    fixes

--- src/routes.php
+++ src/routes.php
@@ -13,20 +13,9 @@ Route::group(array(
             $route->any('/{tag_id}/edit', array("as" => "admin.tags.edit", "uses" => "Dot\Tags\Controllers\TagsController@edit"));
             $route->any('/delete', array("as" => "admin.tags.delete", "uses" => "Dot\Tags\Controllers\TagsController@delete"));
             $route->any('/search', array("as" => "admin.tags.search", "uses" => "Dot\Tags\Controllers\TagsController@search"));
-            $route->any('/migration', array("as" => "admin.tags.migration", "uses" => "Dot\Tags\Controllers\TagsController@migration"));
-            $route->get('google/keywords', array("as" => "admin.google.search", "uses" =>"Dot\\ServicesController@keywords"));
         });
 });
 
-Route::group(array(
-), function($route) {
-    $route->group(array("prefix" => "tags"), function($route) {
-        $route->get('migration/{offset?}', array("as" => "admin.tags.mig", "uses" => "Dot\Tags\Controllers\TagsController@migrate"));
-        $route->get('private_tags/{offset?}', array("as" => "admin.tags.private_tags", "uses" => "Dot\Tags\Controllers\TagsController@private_tags"));
-        $route->get('duplicated_tags/{offset?}', array("as" => "admin.tags.duplicated_tags", "uses" => "Dot\Tags\Controllers\TagsController@duplicated_tags"));
-        $route->get('script/{offset?}', array("as" => "admin.tags.script", "uses" => "Dot\Tags\Controllers\TagsController@script"));
-    });
-});
 
 /*
  * API
